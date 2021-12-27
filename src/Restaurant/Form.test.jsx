167f0f030e230cd45c5533f324d10ad35b5b5d76
padawanR0k <padawanR0k@gmail.com>
2021-12-27 20:48:37 +0900

    up: 의미가 드러나지 않은 시나리오 수정

--- src/Restaurant/Form.test.jsx
+++ src/Restaurant/Form.test.jsx
@@ -2,7 +2,7 @@ import { render, fireEvent } from '@testing-library/react';
 import Form from './Form';
 
 describe('Form', () => {
-  test('onClick', () => {
+  it('등록버튼을 누르면 props로 전달된 함수를 실행한다.', () => {
     const onClick = jest.fn();
 
     const { getByText } = render(<Form onClick={onClick} />);
@@ -12,7 +12,7 @@ describe('Form', () => {
     expect(onClick).toBeCalled();
   });
 
-  test('props', () => {
+  it('props로 전달된 내용들을 폼에 출력한다.', () => {
     const onChange = jest.fn();
     const { getByDisplayValue } = render(
       <Form
